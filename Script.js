60872f6747d5127c258d4a2c2a96b400f1341094
sailenevesramos@gmail.com <SaileNeves>
2023-03-14 21:01:21 -0300

    Co-authored-by: Patrick Delaney <patdel0@users.noreply.github.com>

---
+++ Script.js
@@ -0,0 +1,22 @@
+// add
+function addNumbers(num1,num2){
+    let result = num1 + num2
+    return result
+}
+
+// subtract
+function subtNumbers(num1,num2){
+    if (typeof num1 === "number" && typeof num2 === "number"){;
+    return num1 - num2;}
+    else {console.log('Error:Unexpected type provided')}
+}
+
+// division
+let divNumber = function(num1,num2){
+    return  num1 / num2
+}
+    
+// multiplication
+let multpNumber = function(num1,num2){
+    return num1 * num2
+}
