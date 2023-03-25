058a88000315684c7895a43637a7ebabfb18b41b
sailenevesramos@gmail.com <SaileNeves>
2023-03-24 21:02:58 -0300

    finished cards started working on sorting Co-authored-by: Patrick Delaney <patdel0@users.noreply.github.com>

--- 3Movie Data/MovieData.js
+++ 3Movie Data/MovieData.js
@@ -1,102 +1,121 @@
 let movieData = {
-    "The Darjeeling Limited": {
-        plot: "A year after their father's funeral, three brothers travel across India by train in an attempt to bond with each other.",
-        cast: ["Jason Schwartzman", "Owen Wilson", "Adrien Brody"],
-        runtime: 151,
-        rating: 7.2,
-        year: 2007,
-    },
-    "The Royal Tenenbaums": {
-        plot: "The eccentric members of a dysfunctional family reluctantly gather under the same roof for various reasons",
-        rating: 7.6,
-        year: 2001,
-        cast: ["Gene Hackman", "Gwnyeth Paltrow", "Anjelica Huston"],
-        runtime: 170,
-    },
-    "Fantastic Mr. Fox": {
-        year: 2009,
-        plot: "An urbane fox cannot resist returning to his farm raiding ways and then must help his community survive the farmers' retaliation.",
-        cast: [
-            "George Clooney",
-            "Meryl Streep",
-            "Bill Murray",
-            "Jason Schwartzman",
-        ],
-        runtime: 147,
-        rating: 7.9,
-    },
-    "The Grand Budapest Hotel": {
-        rating: 8.1,
-        runtime: 159,
-        year: 2014,
-        plot: "A writer encounters the owner of an aging high-class hotel, who tells him of his early years serving as a lobby boy in the hotel's glorious years under an exceptional concierge.",
-        cast: ["Ralph Fiennes", "F. Murray Abraham", "Mathieu Amalric"],
-    },
+  "The Darjeeling Limited": {
+    plot: "A year after their father's funeral, three brothers travel across India by train in an attempt to bond with each other.",
+    cast: ["Jason Schwartzman", "Owen Wilson", "Adrien Brody"],
+    runtime: 151,
+    rating: 7.2,
+    year: 2007,
+  },
+  "The Royal Tenenbaums": {
+    plot: "The eccentric members of a dysfunctional family reluctantly gather under the same roof for various reasons",
+    rating: 7.6,
+    year: 2001,
+    cast: ["Gene Hackman", "Gwnyeth Paltrow", "Anjelica Huston"],
+    runtime: 170,
+  },
+  "Fantastic Mr. Fox": {
+    year: 2009,
+    plot: "An urbane fox cannot resist returning to his farm raiding ways and then must help his community survive the farmers' retaliation.",
+    cast: [
+      "George Clooney",
+      "Meryl Streep",
+      "Bill Murray",
+      "Jason Schwartzman",
+    ],
+    runtime: 147,
+    rating: 7.9,
+  },
+  "The Grand Budapest Hotel": {
+    rating: 8.1,
+    runtime: 159,
+    year: 2014,
+    plot: "A writer encounters the owner of an aging high-class hotel, who tells him of his early years serving as a lobby boy in the hotel's glorious years under an exceptional concierge.",
+    cast: ["Ralph Fiennes", "F. Murray Abraham", "Mathieu Amalric"],
+  },
 };
 
-    const movieTitles = Object.keys(movieData);
-    const movieObjs = Object.values(movieData);
-    
-    for(let i = 0; i < 4; i++) {
-        // Create title
-        const movieTitle = movieTitles[i]; 
-        //Create storage location
-        const newCard = document.createElement('div');
-        newCard.classList.add('movie-card');
-    
-        const NewTitle = document.createElement('h2')
-        NewTitle.textContent = `Movie: ${movieTitle}`;
-        newCard.appendChild(NewTitle);
-      
-        // Movie object
-        const MovieInfo = movieObjs[i];
+const movieTitles = Object.keys(movieData);
+const movieObjs = Object.values(movieData);
+
+for (let i = 0; i < 4; i++) {
+  // Create title
+  const movieTitle = movieTitles[i];
+  //Create storage location
+  const newCard = document.createElement('div');
+  newCard.classList.add('movie-card');
+
+  const NewTitle = document.createElement('h2')
+  NewTitle.textContent = `Movie: ${movieTitle}`;
+  newCard.appendChild(NewTitle);
+
+  // Movie object
+  const MovieInfo = movieObjs[i];
+
+  createParagraph('year', newCard, MovieInfo)
+  createParagraph('plot', newCard, MovieInfo)
+  createParagraph('cast', newCard, MovieInfo)
+  createParagraph('runtime', newCard, MovieInfo)
+  createParagraph('rating', newCard, MovieInfo)
   
-        // Create elements for properties
-        for(let property in MovieInfo) {
-            createParagraph(property, newCard, MovieInfo);
-        }
+  // Append new card to parent
+  const parentElement = document.querySelector('.content');
+  parentElement.appendChild(newCard);
+}
 
-        // Append new card to parent
-        const parentElement = document.querySelector('.content');
-        parentElement.appendChild(newCard);
-    }
-
-    
-function createParagraph(propertyName, newCard, filmeInfo){
-    const newP = document.createElement('p');
-    newP.textContent = `${propertyName}: ${filmeInfo[propertyName]}`;
-    newCard.appendChild(newP);
+function createParagraph(propertyName, newCard, filmeInfo) {
+  const newP = document.createElement('p');
+  newP.textContent = `${propertyName}: ${filmeInfo[propertyName]}`;
+  newCard.appendChild(newP);
 }
 
 //Show/hide Form button
 function toggleForm() {
-    let form = document.getElementById("movie-form");
-    if (form.style.display === "none") {
-      form.style.display = "block";
-    } else {
-      form.style.display = "none";
-    }
+  let form = document.getElementById("movie-form");
+  if (form.style.display === "none") {
+    form.style.display = "block";
+  } else {
+    form.style.display = "none";
   }
+}
 
-  function createMovieElement() {
-    const newCard = document.createElement("div");
+/* const newCard = document.createElement("div"); */
 
-    let Title = document.getElementById("title-value").value;
-    let Rating = document.getElementById("rating-value").value;
-    console.log(Title)
-  }
+function createMovieElement(elementType, propertyName, newCardArg) {
+  const NewElement = document.createElement(elementType);
+  const inputValue = document.getElementById(`${propertyName}-value`).value;
+  NewElement.textContent = `${propertyName}: ${inputValue}`;
+  newCardArg.appendChild(NewElement)
+}
 
+function createCard() {
+  const newCard = document.createElement("div");
+  newCard.classList.add('movie-card');
+  createMovieElement('h2', 'title', newCard);
+  createMovieElement('p', 'year', newCard);
+  createMovieElement('p', 'plot', newCard);
+  createMovieElement('p', 'cast', newCard);
+  createMovieElement('p', 'runtime', newCard);
+  createMovieElement('p', 'rating', newCard);
+  const parentElement = document.querySelector('.content');
+  parentElement.appendChild(newCard);
+}
 
-  //Submit button
-  function createCard(event) {
-    event.preventDefault();
+//Submit button
+function submitCard(event) {
+  event.preventDefault();
+  createCard();
+  clearForm();
+}
 
-    const newCard = document.createElement("div");
-    createMovieElement('title', 'h2', newCard);
-    createMovieElement('rating', 'p', newCard);
-    clearForm()
-  }
+//Clear Form
+function clearForm() {
+  document.getElementById("movie-form").reset();
+}
 
-  function clearForm() {
-    document.getElementById("movie-form").reset();
-  }
+const cards = document.querySelectorAll('.movie-card');
+const yearElements = [];
+cards.forEach(card => {
+  const yearElement = card.querySelector('.year');
+  yearElements.push(yearElement);
+});
+
